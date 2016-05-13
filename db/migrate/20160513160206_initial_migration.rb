0bd4d17699f98b8dd902fbb254da67a6207de2a5
RJ Dellecese <rjdellecese@gmail.com>
2016-05-13 16:30:27 +0000

    Create initial migration sans user table.

---
+++ db/migrate/20160513160206_initial_migration.rb
@@ -0,0 +1,30 @@
+class InitialMigration < ActiveRecord::Migration
+  def change
+    create_table :links do |t|
+      t.string :uri
+      t.integer :upvote_count
+      t.integer :downvote_count
+
+      t.timestamps null: false
+    end
+    
+    create_table :tags do |t|
+      t.string :name
+      t.integer :sequence
+
+      t.timestamps null: false
+    end
+    
+    create_table :links_tags do |t|
+      t.belongs_to :links, index: true
+      t.belongs_to :tags, index: true
+    end
+    
+    create_table :comments do |t|
+      t.belongs_to :link, index: true
+      t.string :body
+      
+      t.timestamps null: false
+    end
+  end
+end
