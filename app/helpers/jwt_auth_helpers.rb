fcd66c2ed5b983e0767a939e49f5ae4a623e38d9
Oleg Bulkin <o.bulkin@gmail.com>
2016-06-29 01:57:58 +0000

    Created a module with Grape helpers for authenticating a user based on a JWT provided in an Authorization header

---
+++ app/helpers/jwt_auth_helpers.rb
@@ -0,0 +1,17 @@
+module JWTAuthHelpers
+  def current_user
+   user_info_from_header ? User.find_by(id:user_info_from_header.user_id) : nil
+  rescue JWT::DecodeError
+    nil
+  end
+
+  def authenticate_user!
+    error!({message: "Authentication failure"}, 401) unless current_user
+  end
+
+  private
+  def user_info_from_header
+    return nil unless @user_info || (headers["Authorization"] && headers["Authorization"].split(" ").first == "Bearer")
+    @user_info ||= AuthToken.decode headers["Authorization"].split(" ").last
+  end
+end
